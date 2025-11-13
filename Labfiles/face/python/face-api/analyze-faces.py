c1bed4fecd6657129b9702f4b25ff55f654dc8d9
armandolopezo <armandolopezo@hotmail.com>
2025-11-13 13:25:25 -0400

    Get faces code

--- Labfiles/face/python/face-api/analyze-faces.py
+++ Labfiles/face/python/face-api/analyze-faces.py
@@ -38,8 +38,37 @@ def main():
                     FaceAttributeTypeDetection01.ACCESSORIES]
 
         # Get faces
- 
- 
+        with open(image_file, mode="rb") as image_data:
+            detected_faces = face_client.detect(
+                image_content=image_data.read(),
+                detection_model=FaceDetectionModel.DETECTION01,
+                recognition_model=FaceRecognitionModel.RECOGNITION01,
+                return_face_id=False,
+                return_face_attributes=features,
+            )
+        face_count = 0
+        if len(detected_faces) > 0:
+            print(len(detected_faces), 'faces detected.')
+            for face in detected_faces:
+            
+                # Get face properties
+                face_count += 1
+                print('\nFace number {}'.format(face_count))
+                print(' - Head Pose (Yaw): {}'.format(face.face_attributes.head_pose.yaw))
+                print(' - Head Pose (Pitch): {}'.format(face.face_attributes.head_pose.pitch))
+                print(' - Head Pose (Roll): {}'.format(face.face_attributes.head_pose.roll))
+                print(' - Forehead occluded?: 
+        {}'.format(face.face_attributes.occlusion["foreheadOccluded"]))
+                print(' - Eye occluded?: 
+        {}'.format(face.face_attributes.occlusion["eyeOccluded"]))
+                print(' - Mouth occluded?: 
+        {}'.format(face.face_attributes.occlusion["mouthOccluded"]))
+                print(' - Accessories:')
+                for accessory in face.face_attributes.accessories:
+                    print('   - {}'.format(accessory.type))
+                # Annotate faces in the image
+                annotate_faces(image_file, detected_faces)
+            
 
     except Exception as ex:
         print(ex)
